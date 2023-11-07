2f1662fe88a8a4155ab65489dd2d0f8f865a9c87
Hovhannes Tsakanyan <hovhannes.ht@gmail.com>
2023-11-07 15:46:02 +0400

    Added windows specific part for a testcase

--- test/regression/conftest.py
+++ test/regression/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import subprocess
 import time
+import sys
+import os
 
 import pyscreeze
 import PIL
@@ -18,16 +20,55 @@ def app_path(request):
     name_value = request.config.option.app
     if name_value is None:
         pytest.skip()
-    return name_value+".app"
+    return name_value.replace('/', os.path.sep).replace('\\', os.path.sep)
+
+def closeAll(app_path):
+    if sys.platform == "win32":  # Windows
+        pg = os.path.join(os.path.dirname(__file__), "kill_process.ps1")
+        subprocess.run(["powershell", pg , app_path], shell=True, check=True)
+    else:
+        subprocess.run(["killall", "-w", app_path], shell=True)
+
+
+def get_resource(path):
+    if sys.platform == "win32":
+        suffix = "_win"
+    elif sys.platform == "darwin":
+        suffix = "_mac"
+    else:
+        suffix = "_linux"
+
+    # Split the path into its components
+    dir_name, base_name = os.path.split(path)
+
+    # Split the filename and its extension
+    file_name, file_extension = os.path.splitext(base_name)
+
+    # Add the suffix to the filename
+    new_file_name = file_name + suffix + file_extension
+
+    # Join the path back together
+    new_path = os.path.join(dir_name, new_file_name)
+
+    # Check if the new_path exists
+    if os.path.exists(new_path):
+        return new_path
+    else:
+        return path
+
+@pytest.fixture
+def os_resource_getter():
+    return get_resource
+
 
 @pytest.fixture(scope="function")
 def start_application(app_path):
-    quit_process = subprocess.Popen(f"osascript -e 'quit app \"{app_path}\"'", shell=True)
-    quit_process.wait()
-    app_process = subprocess.Popen(["open", "-W", "-a", app_path])
+    open_args = [app_path]
+    if sys.platform == "darwin":
+        open_args = ["open", "-W", "-a", app_path]
+    closeAll(app_path)
+    app_process = subprocess.Popen(open_args)
     # Allow some time for the application to start (you may need to adjust the sleep duration)
     time.sleep(1)
     yield app_process
-    # Teardown: Terminate the application after the test
-    quit_process = subprocess.Popen(f"osascript -e 'quit app \"{app_path}\"'", shell=True)
-    quit_process.wait()
+    closeAll(app_path)
